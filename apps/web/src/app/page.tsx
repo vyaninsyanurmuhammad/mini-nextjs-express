4b66033953123a0777b785d20dc84ac38f0c561e
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-04-06 19:37:59 +0700

    feat(shadcn): :package: add shacdn ui component packages and configurations

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,12 @@
+import HomeNavbar from '@/components/home-navbar';
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <>
+      <div className="flex flex-col">
+        <HomeNavbar />
+        <main>Home</main>
+      </div>
+    </>
+  );
 }
